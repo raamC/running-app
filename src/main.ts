bb1d63e9c911610e33df6434b2aaded90e51adef
Raam Chauhan <raam.chauhan@softwire.com>
2018-07-31 10:39:34 +0100

    Go back to using semi-colons.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from '@/router'
-import store from '@/store'
-import * as env from '../env'
-import * as VueGoogleMaps from 'vue2-google-maps'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue';
+import App from './App.vue';
+import router from '@/router';
+import store from '@/store';
+import * as env from '../env';
+import * as VueGoogleMaps from 'vue2-google-maps';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 Vue.use(VueGoogleMaps, {
   load: {
     key: env.API_KEY,
     libraries: 'geometry',
   },
-})
+});
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,15 +6,15 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import GoogleMap from '@/components/GoogleMap.vue' // @ is an alias to /src
-import Controller from '@/components/Controller.vue'
+import { Component, Vue } from "vue-property-decorator";
+import GoogleMap from "@/components/GoogleMap.vue"; // @ is an alias to /src
+import Controller from "@/components/Controller.vue";
 
 @Component({
   components: {
     GoogleMap,
-    Controller,
-  },
+    Controller
+  }
 })
 export default class Home extends Vue {}
 </script>
@@ -22,13 +22,12 @@ export default class Home extends Vue {}
 <style lang="scss" scoped >
 @import "../global.scss";
 
-  .home {
-    display: flex;
-    margin: 100px;
-  }
+.home {
+  display: flex;
+  margin: 100px;
+}
 
-  .map {
-    margin-right: 100px;
-  }
-
+.map {
+  margin-right: 100px;
+}
 </style>
