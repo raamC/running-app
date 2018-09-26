9031feaab4bd72a54e763f798a71edb55036e1f2
Raam Chauhan <raam.chauhan@softwire.com>
2018-09-26 11:03:45 +0100

    Pull run data from Firestore.

--- src/views/SavedRuns.vue
+++ src/views/SavedRuns.vue
@@ -4,13 +4,19 @@
     <div class='savedRuns'>
         <h1>Saved Runs go here</h1>
     </div>
-  </div>
+      <ul >
+        <li :key='i' v-for='(run, i) in runData'>
+          {{ run }}
+        </li>
+      </ul>
+    </div>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator';
 import NavBar from '@/components/NavBar.vue';
 import firebase from 'firebase';
+import 'firebase/firestore';
 
 @Component({
   components: {
@@ -18,8 +24,22 @@ import firebase from 'firebase';
   },
 })
 export default class SavedRuns extends Vue {
+  private db: any = firebase.firestore();
+  private runData: any[] = [];
   // Data properties
   // Lifecycle hooks
+  private mounted() {
+    this.db.collection('runs').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        this.runData.push(doc.data())
+      });
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
+  };
+  
   // Computed value
   // Component methods
 }
